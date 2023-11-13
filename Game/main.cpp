271c1f776f362abddc897b7ff5cb064f7563d107
Santiago Salazar <72097025+SFenrir20@users.noreply.github.com>
2023-11-13 00:30:35 -0500

    feat: :sparkles: se creo nuevas funciones
    se creo funciones para leer los txt de la data, para la creacion de los personajes

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
+  string rutaArchivo = "C:/Users/santi/OneDrive/Documents/Repositorios/Estructuras y Programacion de Algoritmos/ProyectoFinal_D-D/Data";
+
 
    int options;
    cout << " Bienvenido a un Nueva Aventura" << endl;
@@ -12,12 +14,16 @@ int main(){
    cin >> options;
 
    switch(options)  {
-    case 1:
+    case 1:{
       cout << "Has seleccionado ser Game Master" << endl;
-      break;
-    case 2:
-      cout << "Has selecccionado ser Juegador" << endl;
-      break;
+      break;}
+    case 2:{
+      cout << "Has selecccionado ser Jugador" << endl;
+       Personaje jugador = crearPersonaje(rutaArchivo); // Llamada a la función crearPersonaje
+       cout << "¡Personaje creado! Nombre: " << jugador.nombre 
+            << ", Raza: " << jugador.raza 
+            << ", Clase: " << jugador.clase << endl;
+      break;}
     default:
       cout << "Opción no valida. Por favor elija ser GM o Jugador" << endl;
       break;
--- Game/personajes.h
+++ Game/personajes.h
@@ -1,4 +1,11 @@
+// personajes.h
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+#include <fstream>
+#include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -11,21 +18,81 @@ struct Personaje {
  float puntos_exp;
  float puntos_def;
  float puntos_mag;
-
 };
-inline Personaje crearPersonaje(){
+
+
+// Función para leer las opciones desde un archivo de texto
+inline vector<string> leerOpcionesDesdeArchivo(const string& rutaArchivo){
+    vector<string> opciones;
+    ifstream archivo(rutaArchivo);
+
+    if (archivo.is_open()){
+         string linea;
+         while (getline(archivo, linea, ';')){
+            opciones.push_back(linea);
+         }
+         archivo.close();
+    }else {
+        cerr << "No se pudo abrir el archivo: " << rutaArchivo << endl;
+    }
+    return opciones;
+}
+// Función para mostrar las opciones disponibles
+inline void mostrarOpciones(const vector<string>& opciones) {
+    cout << "Opciones disponibles:" << endl;
+    for (size_t i = 0; i < opciones.size(); ++i) {
+        cout << i + 1 << ". " << opciones[i] << endl;
+    }
+}
+
+// Función para que el jugador elija una opción
+inline string elegirOpcion(const vector<string>& opciones) {
+    mostrarOpciones(opciones);
+
+    int eleccion;
+    cin >> eleccion;
+
+    // Validar la elección
+    if (eleccion < 1 || eleccion > static_cast<int>(opciones.size())) {
+        cerr << "Elección no valida." << endl;
+        // Puedes manejar esto de diferentes maneras, por ejemplo, volver a pedir la elección.
+        return "";
+    }
+
+    return opciones[eleccion - 1];
+}
+
+// Función para que el jugador elija una raza
+inline string elegirRaza(const string& rutaArchivo) {
+    vector<string> razas = leerOpcionesDesdeArchivo( rutaArchivo + "\\Razas.txt");
+    cout << "Elige una raza para tu personaje:" << endl;
+    return elegirOpcion(razas);
+}
+
+// Función para que el jugador elija una clase
+inline string elegirClase(const string& rutaArchivo) {
+    vector<string> clases = leerOpcionesDesdeArchivo(rutaArchivo + "/Clases.txt");
+    cout << "Elige una clase para tu personaje:" << endl;
+    return elegirOpcion(clases);
+}
+
+
+// Función para crear un personaje con raza y clase elegidas
+inline Personaje crearPersonaje(const string& rutaArchivo) {
     Personaje nuevoPersonaje;
     cout << "!Creemos tu Personaje" << endl;
-    cout << "ingresa el  nombre de tu personaje" << endl;
+    cout << "Ingresa el nombre de tu personaje" << endl;
     cin >> nuevoPersonaje.nombre;
 
-    cout << "Seleciona una clase para tu personaje" << endl;
-    cin >> nuevoPersonaje.clase; 
+    nuevoPersonaje.raza = elegirRaza(rutaArchivo);
+    nuevoPersonaje.clase = elegirClase(rutaArchivo);
 
-    //esto se debe hacer de manera automartica para cada clase o hacerlo con dados 
-    //crear una funcion que se use dados para la genracion de los puntos del pj  
+    // Esto se debe hacer de manera automática para cada clase o hacerlo con dados
+    // Crear una función que use dados para la generación de los puntos del pj
     nuevoPersonaje.nivel = 1;
-    nuevoPersonaje.puntos_vida = 10;  
+    nuevoPersonaje.puntos_vida = 10;
 
     return nuevoPersonaje;
 }
+
+#endif // PERSONAJE_H
