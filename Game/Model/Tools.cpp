4df128f1250e9baa3f14aff5c28756b9d002e238
d-a-b-o <bravodavid451@gmail.com>
2023-11-21 09:50:12 -0500

    feat(Tools): :art: Ordenado tools

--- Game/Model/Tools.cpp
+++ Game/Model/Tools.cpp
@@ -124,3 +124,94 @@ void Tools::dado10Caras()
 
     system("pause");
 }
+bool Tools::esNumeroValido(int numero, int min, int max)
+{
+    return (numero >= min && numero <= max);
+}
+bool Tools::validarNumero()
+{
+    int numero;
+    cout << "\t>";
+    cin >> numero;
+
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return false;
+    }
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    return true;
+}
+bool Tools::esCadenaValida(const std::string &cadena)
+{
+    for (char c : cadena)
+    {
+        if (!isalpha(c))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool Tools::validarCadena()
+{
+    string input;
+    cout << "Ingresa una cadena: ";
+    getline(cin, input);
+
+    return esCadenaValida(input);
+}
+char Tools::obtenerOpcionValida()
+{
+    char opcion;
+    cout << "Ingresa una opción: ";
+    cin >> opcion;
+    opcion = toupper(opcion);
+
+    while (opcion != 'A' && opcion != 'B' && opcion != 'C')
+    {
+        cout << "Opción inválida. Ingresa A, B o C: ";
+        cin >> opcion;
+        opcion = toupper(opcion);
+    }
+
+    return opcion;
+}
+bool Tools::esDoubleValido(double numero, double min, double max)
+{
+    return (numero >= min && numero <= max);
+}
+bool Tools::validarDouble()
+{
+    double numero;
+    cout << "Ingresa un número decimal: ";
+    cin >> numero;
+
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
+    }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return true;
+}
+bool Tools::esCadenaLongitudValida(const string &cadena, size_t longitud)
+{
+    return (cadena.length() == longitud);
+}
+bool Tools::validarCadenaLongitud()
+{
+    string input;
+    cout << "Ingresa una cadena de longitud específica: ";
+    cin >> input;
+
+    const size_t longitud_esperada = 10;
+
+    return esCadenaLongitudValida(input, longitud_esperada);
+}
--- Game/Model/Tools.h
+++ Game/Model/Tools.h
@@ -16,99 +16,13 @@ public:
     void dado6Caras();
     void dado8Caras();
     void dado10Caras();
-    bool esNumeroValido(int numero, int min, int max)
-    {
-        return (numero >= min && numero <= max);
-    }
-
-    bool validarNumero()
-    {
-        int numero;
-        std::cout << "Ingresa un número: ";
-        std::cin >> numero;
-
-        if (std::cin.fail())
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            return false;
-        }
-
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        return true;
-    }
-    bool esCadenaValida(const std::string &cadena)
-    {
-        for (char c : cadena)
-        {
-            if (!isalpha(c))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool validarCadena()
-    {
-        std::string input;
-        std::cout << "Ingresa una cadena: ";
-        std::getline(std::cin, input);
-
-        return esCadenaValida(input);
-    }
-    char obtenerOpcionValida()
-    {
-        char opcion;
-        std::cout << "Ingresa una opción: ";
-        std::cin >> opcion;
-        opcion = std::toupper(opcion);
-
-        while (opcion != 'A' && opcion != 'B' && opcion != 'C')
-        {
-            std::cout << "Opción inválida. Ingresa A, B o C: ";
-            std::cin >> opcion;
-            opcion = toupper(opcion);
-        }
-
-        return opcion;
-    }
-    bool esDoubleValido(double numero, double min, double max)
-    {
-        return (numero >= min && numero <= max);
-    }
-
-    bool validarDouble()
-    {
-        double numero;
-        std::cout << "Ingresa un número decimal: ";
-        std::cin >> numero;
-
-        if (std::cin.fail())
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            return false;
-        }
-
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        return true;
-    }
-    bool esCadenaLongitudValida(const std::string &cadena, size_t longitud)
-    {
-        return (cadena.length() == longitud);
-    }
-
-    bool validarCadenaLongitud()
-    {
-        std::string input;
-        std::cout << "Ingresa una cadena de longitud específica: ";
-        std::cin >> input;
-
-        const size_t longitud_esperada = 10;
-
-        return esCadenaLongitudValida(input, longitud_esperada);
-    }
+    bool esNumeroValido(int, int, int);
+    bool validarNumero();
+    bool esCadenaValida(const string &);
+    bool validarCadena();
+    char obtenerOpcionValida();
+    bool esDoubleValido(double, double, double);
+    bool validarDouble();
+    bool esCadenaLongitudValida(const string &, size_t);
+    bool validarCadenaLongitud();
 };
